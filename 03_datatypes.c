663ef5c9533af855cdd272c7a36aff640f7922f2
AnshMishra033 <princethegreat0033@gmail.com>
2026-06-03 14:33:30 +0530

    Add files via upload

---
+++ 03_datatypes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a=8;
+    printf("THE VALUE OF a IS  %d\n",a);
+    float b=1.2;
+    printf("THE VALUE OF b IS %f\n",b);
+    char c;
+    c='a';
+    printf("THE VALUE OF c IS %c\n",c);
+    return 0;
+}
---
+++ 04_comments.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    // I am writing this for fun 
+    printf("Hello World");
+    return 0;
+}
